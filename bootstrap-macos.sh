5903e74c0ecfa52204606532391b8f9dc4bbbbe0
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-09-27 16:36:47 -0600

    be more explicit about file locations

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-CURRENTDIR=$(dirname "$0")
 rm -rf "$HOME/.zprezto"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
 chsh -s /bin/zsh
-ln -fs "$CURRENTDIR/vimrc" "$HOME/.vimrc"
-ln -fs "$CURRENTDIR/zpreztorc-macos" "$HOME/.zpreztorc"
-ln -fs "$CURRENTDIR/zprofile" "$HOME/.zprofile"
-ln -fs "$CURRENTDIR/zshrc" "$HOME/.zshrc"
-ln -fs "$CURRENTDIR/zlogin" "$HOME/.zlogin"
-ln -fs "$CURRENTDIR/zlogout" "$HOME/.zlogout"
-ln -fs "$CURRENTDIR/zshenv" "$HOME/.zshenv"
+ln -fs "$HOME/dotfiles/vimrc" "$HOME/.vimrc"
+ln -fs "$HOME/dotfiles/zpreztorc-macos" "$HOME/.zpreztorc"
+ln -fs "$HOME/dotfiles/zprofile" "$HOME/.zprofile"
+ln -fs "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
+ln -fs "$HOME/dotfiles/zlogin" "$HOME/.zlogin"
+ln -fs "$HOME/dotfiles/zlogout" "$HOME/.zlogout"
+ln -fs "$HOME/dotfiles/zshenv" "$HOME/.zshenv"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git go zsh cloudfoundry/tap/cf-cli cloudfoundry/tap/bosh-cli direnv yarn wget tree tmux tcptraceroute jq
 git config --global user.email "jfitzgerald@pivotal.io"

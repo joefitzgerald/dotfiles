1afab1cae3f5a75a23b67af4aea52ec829b79dd3
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-11-02 19:52:43 -0600

    allow for cleanup on linux

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -14,10 +14,15 @@ if [[ "$OSTYPE" == darwin* ]]; then
 else
   wget -O go.tar.gz https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
 fi
-rm -rf /usr/local/go
+sudo rm -rf /usr/local/go
+sudo mkdir -p /usr/local/go
+sudo chown joe:joe /usr/local/go
 tar -C /usr/local -xzf go.tar.gz
-
+rm -rf go.tar.gz
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+rm -rf ~/.vim/plugged/vim-go
+rm -rf ~/.vim/plugged/molokai
+rm -rf ~/.zprezto
 git clone https://github.com/fatih/vim-go.git ~/.vim/plugged/vim-go
 git clone https://github.com/fatih/molokai.git ~/.vim/plugged/molokai
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
@@ -28,7 +33,7 @@ ln -fs "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
 ln -fs "$HOME/dotfiles/zlogin" "$HOME/.zlogin"
 ln -fs "$HOME/dotfiles/zlogout" "$HOME/.zlogout"
 ln -fs "$HOME/dotfiles/zshenv" "$HOME/.zshenv"
-mkdir "$HOME/go"
+mkdir -p "$HOME/go"
 git config --global user.email "jfitzgerald@pivotal.io"
 git config --global user.name "Joe Fitzgerald"
 git config --global pull.rebase true
